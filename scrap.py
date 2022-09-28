91c12eda6e4a1167fc9d3315c485488464c64cff
Ivandros Awom <norvin2911@gmail.com>
2022-09-28 02:13:56 -0500

    Add prcie, location and links

--- scrap.py
+++ scrap.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from bs4 import BeautifulSoup
 import requests
 
@@ -7,15 +8,31 @@ soup = BeautifulSoup(html_text,'lxml')
 jobs = soup.find_all('div', class_ = 'unit-body')
 
 for job in jobs:
+    #Make
     make_and_year = job.find('span', class_ = 'ymm').text.split()
     make=''
     for i in range(len(make_and_year)):
         if i+1 < len(make_and_year):
             make += make_and_year[i+1]+" "
-                
+
+    #Year            
     year = make_and_year[0]
+    #Category
     category = job.find('span', class_ = 'type-name').text
+    #Odometer
     odometer = job.find('div', class_ = 'item odom').text
-    print(make,year,category,odometer)
+    #Price
+    price = NULL
+    if job.find('div', class_='item price') is not None:
+        price = job.find('div', class_='item price').text
+    
+    #Location
+    location = job.find('span', class_ = 'dealer-loc').text.split(', ')
+    city = location[0]
+    state = location[1]
+    #link
+    sub_link = job.find('a', {'class': 'spec-link'})['href']
+    link = 'https://www.soarr.com'+sub_link
+    print(make,year,category,odometer, price, city, state, link)
 
 print('DATA COMPLETE')
