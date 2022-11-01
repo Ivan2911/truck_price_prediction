0dfc3cdf4a63329d32eb5d036c6619a6fb817084
Ivandros Awom <norvin2911@gmail.com>
2022-11-01 01:15:47 -0500

    Rename app to application for AWS pipline

--- application.py
+++ application.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from flask import Flask, render_template, request, redirect, session
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def home():
     return render_template('home.html')
 
-@app.route("/generate_report", methods=['POST'])
+@application.route("/generate_report", methods=['POST'])
 def output_calc():
     return "Truck Valuation and Comparison Report"
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
     
